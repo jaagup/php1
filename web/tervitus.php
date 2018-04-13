47260ce2d3a28e197eb347a3fdb7ecb632f6b587
Jaagup Kippar <jaagup@tlu.ee>
2018-04-13 17:23:22 +0300

    Update tervitus.php

--- web/tervitus.php
+++ web/tervitus.php
@@ -1,8 +1,6 @@
-abc<?php
-/*
+<?php
   if(isSet($_REQUEST["eesnimi"])){
      echo "Tere, $_REQUEST[eesnimi]";
   } else {
      echo "Void aadressireale panna eesnime parameetri alla enese oma";
   }
-*/
